9755b076f9e636e223d84ef14e5725474bdbea8f
Arief Sibuea <sibuearief@gmail.com>
2021-12-05 18:35:32 +0700

    [leetcode][1032] stream of characters (#1)

---
+++ leetcode/1032-stream_of_characters/solution_test.go
@@ -0,0 +1,26 @@
+package streamofcharacters_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	lib "github.com/ariefsibuea/dsa/leetcode/1032-stream_of_characters"
+)
+
+func Test_StreamOfCharacters(t *testing.T) {
+	words := []string{"cd", "f", "kl"}
+	inputStream := "abcdefghijkl"
+	expectedOutputStream := []bool{false, false, false, true, false, true, false, false, false, false, false, true}
+
+	streamChecker := lib.Constructor(words)
+	actualOutputStream := make([]bool, 0)
+	for _, letter := range []byte(inputStream) {
+		actualOutputStream = append(actualOutputStream, streamChecker.Query(letter))
+	}
+
+	require.Len(t, actualOutputStream, len(expectedOutputStream))
+	for i := range actualOutputStream {
+		require.Equal(t, expectedOutputStream[i], actualOutputStream[i])
+	}
+}
