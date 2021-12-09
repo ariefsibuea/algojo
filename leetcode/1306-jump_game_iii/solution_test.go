fa15ba0cee95b9a88833f411cbbc6229713a5136
Arief Sibuea <sibuearief@gmail.com>
2021-12-10 05:01:35 +0700

    [leetcode][1306] jump game iii (#7)

---
+++ leetcode/1306-jump_game_iii/solution_test.go
@@ -0,0 +1,40 @@
+package jumpgameiii_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	lib "github.com/ariefsibuea/cobra/leetcode/1306-jump_game_iii"
+)
+
+func Test_CanReach(t *testing.T) {
+	testcases := []struct {
+		arr    []int
+		start  int
+		output bool
+	}{
+		{
+			arr:    []int{4, 2, 3, 0, 3, 1, 2},
+			start:  5,
+			output: true,
+		},
+		{
+			arr:    []int{4, 2, 3, 0, 3, 1, 2},
+			start:  0,
+			output: true,
+		},
+		{
+			arr:    []int{3, 0, 2, 1, 2},
+			start:  2,
+			output: false,
+		},
+	}
+
+	for i, testcase := range testcases {
+		t.Run(fmt.Sprintf("case-%d", i+1), func(t *testing.T) {
+			require.Equal(t, testcase.output, lib.CanReach(testcase.arr, testcase.start))
+		})
+	}
+}
