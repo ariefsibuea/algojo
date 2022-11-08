49a92e71095f4b12fbc53aa821e30b2c7e795f55
Arief Sibuea <sibuearief@gmail.com>
2022-11-08 21:06:44 +0700

    hackerrank: solve me first (#2)

---
+++ hackerrank/solve_me_first.go
@@ -0,0 +1,10 @@
+package hackerrank
+
+// Description:
+// a = 7
+// b = 3
+// return sum of a and b = 10
+
+func SolveMeFirst(a uint32, b uint32) uint32 {
+	return a + b
+}
---
+++ hackerrank/solve_me_first_test.go
@@ -0,0 +1,35 @@
+package hackerrank_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	hf "algojo.ariefsibuea.dev/hackerrank"
+)
+
+func Test_SolveMeFirst(t *testing.T) {
+	testCases := []struct {
+		input struct {
+			a uint32
+			b uint32
+		}
+		output uint32
+	}{
+		{
+			input: struct {
+				a uint32
+				b uint32
+			}{a: 2, b: 3},
+			output: 5,
+		},
+	}
+
+	for i, testCase := range testCases {
+		t.Run(fmt.Sprintf("case-%d", i+1), func(t *testing.T) {
+			out := hf.SolveMeFirst(testCase.input.a, testCase.input.b)
+			require.Equal(t, testCase.output, out)
+		})
+	}
+}
