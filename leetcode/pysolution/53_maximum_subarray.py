21d1c9af1b894543df7e9712d6387219cad683e0
Arief Putra <sibuearief@gmail.com>
2025-07-16 10:32:06 +0700

    problem(leetcode-53): maximum subarray

---
+++ leetcode/pysolution/53_maximum_subarray.py
@@ -0,0 +1,55 @@
+"""
+LeetCode Problem : Maximum Subarray
+Topic            : Array, Divide and Conquer, Dynamic Programming
+Level            : Medium
+URL              : https://leetcode.com/problems/maximum-subarray
+"""
+
+from typing import List
+
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        """Finds the contiguous subarray within a one-dimensional array of numbers which has the largest sum using Kadane's algorithm.
+
+        Args:
+            nums (List[int]): A list of integers.
+
+        Returns:
+            int: The largest sum of any contiguous subarray.
+
+        Time Complexity:
+            O(n): Each element is processed exactly once with constant-time operations.
+
+        Space Complexity:
+            O(1): Only two variables (sum and max_subarray) regardless of input size
+        """
+
+        sum = 0
+        max_subarray = float("-inf")
+
+        for _, num in enumerate(nums):
+            if sum < 0:
+                sum = 0
+            sum += num
+
+            max_subarray = max(max_subarray, sum)
+
+        return int(max_subarray)
+
+
+def run_tests():
+    inputs = {"case_1": [[-2, 1, -3, 4, -1, 2, 1, -5, 4]], "case_2": [[1]], "case_3": [[5, 4, -1, 7, 8]]}
+    outputs = {"case_1": 6, "case_2": 1, "case_3": 23}
+
+    solution = Solution()
+
+    for case, input in inputs.items():
+        result = solution.maxSubArray(input[0])
+        assert result == outputs[case], f"{case}: expected {outputs[case]}, got {result}"
+
+    print("✅ All tests passed!")
+
+
+if __name__ == "__main__":
+    run_tests()
