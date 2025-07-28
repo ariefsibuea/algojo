1a733572efe2436950956e944b7ea440d28d8033
Arief Putra <sibuearief@gmail.com>
2025-07-28 11:29:37 +0700

    interview_prep(caraam): add solution for validate parentheses

--- interview_prep/caraam/coding/validate_parentheses.py
+++ interview_prep/caraam/coding/validate_parentheses.py
@@ -2,6 +2,43 @@ class ValidateParentheses:
     def __init__(self):
         pass
 
+    def solve_by_track(self, parentheses: str) -> bool:
+        """Checks if the input string of parentheses is valid (all opening brackets are properly closed and nested).
+
+        Args:
+            parentheses (str): The string containing parentheses to validate.
+
+        Returns:
+            bool: True if the parentheses are valid, False otherwise.
+
+        Raises:
+            ValueError: If the input string is None.
+
+        Time Complexity:
+            O(n): Where n is the length of the input string.
+
+        Space Complexity:
+            O(n): In the worst case, if all parentheses are opening brackers, then we need to store all closing brackets.
+        """
+        expected_closing = ""
+
+        bracket_pairs = {
+            "(": ")",
+            "[": "]",
+            "{": "}",
+        }
+
+        for char in parentheses:
+            if char in bracket_pairs:
+                expected_closing += bracket_pairs[char]
+            elif char in ")}]":
+                if not expected_closing or char != expected_closing[-1]:
+                    return False
+
+                expected_closing = expected_closing[:-1]
+
+        return expected_closing == ""
+
     def solve_by_stack(self, parentheses: str) -> bool:
         """Checks if the input string of parentheses is valid (all opening brackets are properly closed and nested).
 
@@ -23,7 +60,6 @@ class ValidateParentheses:
         Space Complexity:
             O(n): Due to the use of a stack to store opening brackets.
         """
-
         if parentheses is None:
             raise ValueError("parentheses is empty")
 
@@ -65,7 +101,7 @@ if __name__ == "__main__":
     solution = ValidateParentheses()
 
     for case, input in inputs.items():
-        result = solution.solve_by_stack(input[0])
+        result = solution.solve_by_track(input[0])
         assert result == outputs[case], f"{case}: expected {outputs[case]}, got {result}"
 
     print("✅ All tests passed!")
