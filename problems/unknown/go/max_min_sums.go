a420e6612689ab337c559ef46096360a0b3b8f02
Arief Putra <sibuearief@gmail.com>
2025-11-04 15:48:56 +0700

    update(unknown): max and min sums

--- problems/unknown/go/max_min_sums.go
+++ problems/unknown/go/max_min_sums.go
@@ -24,17 +24,17 @@ import (
  * 					Explanation: Maximum sum is 7 by discarded 5 and 6, minimum sum is 5 by discarded 6 and 7.
  */
 
-type IntMinHeap []int
+type MaxHeap []int
 
-func (h IntMinHeap) Len() int           { return len(h) }
-func (h IntMinHeap) Less(i, j int) bool { return h[i] < h[j] }
-func (h IntMinHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h MaxHeap) Len() int           { return len(h) }
+func (h MaxHeap) Less(i, j int) bool { return h[i] > h[j] }
+func (h MaxHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
 
-func (h *IntMinHeap) Push(x any) {
+func (h *MaxHeap) Push(x any) {
 	*h = append(*h, x.(int))
 }
 
-func (h *IntMinHeap) Pop() any {
+func (h *MaxHeap) Pop() any {
 	old := *h
 	n := len(old)
 	x := old[n-1]
@@ -43,30 +43,76 @@ func (h *IntMinHeap) Pop() any {
 	return x
 }
 
-func maxMinSums(nums []int, k int) []int {
-	if k >= len(nums) {
+type MinHeap []int
+
+func (h MinHeap) Len() int           { return len(h) }
+func (h MinHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h MinHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *MinHeap) Push(x any) {
+	*h = append(*h, x.(int))
+}
+
+func (h *MinHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	old[n-1] = 0
+	*h = old[0 : n-1]
+	return x
+}
+
+func maxMinSums(nums []int, n int, k int) []int {
+	if k >= n {
 		return []int{}
 	}
 
-	minHeap := &IntMinHeap{}
+	totalSum := 0
+	for _, num := range nums {
+		totalSum += num
+	}
+
+	if k == 0 {
+		return []int{totalSum, totalSum}
+	}
+
+	maxHeap := &MaxHeap{}
+	heap.Init(maxHeap)
+
+	for _, num := range nums {
+		if maxHeap.Len() < k {
+			heap.Push(maxHeap, num)
+		} else if num < (*maxHeap)[0] {
+			heap.Pop(maxHeap)
+			heap.Push(maxHeap, num)
+		}
+	}
+
+	sumKSmallest := 0
+	for maxHeap.Len() > 0 {
+		sumKSmallest += heap.Pop(maxHeap).(int)
+	}
+
+	maxSum := totalSum - sumKSmallest
+
+	minHeap := &MinHeap{}
 	heap.Init(minHeap)
 
 	for _, num := range nums {
-		heap.Push(minHeap, num)
-	}
-
-	maxSum, minSum := 0, 0
-	num := 0
-
-	for i := 0; i < len(nums); i++ {
-		num = heap.Pop(minHeap).(int)
-		if i < len(nums)-k {
-			minSum += num
-		}
-		if i >= k {
-			maxSum += num
+		if minHeap.Len() < k {
+			heap.Push(minHeap, num)
+		} else if num > (*minHeap)[0] {
+			heap.Pop(minHeap)
+			heap.Push(minHeap, num)
 		}
 	}
+
+	sumKLargest := 0
+	for minHeap.Len() > 0 {
+		sumKLargest += heap.Pop(minHeap).(int)
+	}
+
+	minSum := totalSum - sumKLargest
 
 	return []int{maxSum, minSum}
 }
@@ -74,56 +120,67 @@ func maxMinSums(nums []int, k int) []int {
 func RunTestMaxMinSums() {
 	testCases := map[string]struct {
 		nums   []int
+		n      int
 		k      int
 		expect []int
 	}{
 		"case-1": {
 			nums:   []int{1, 2, 3, 4, 5},
+			n:      5,
 			k:      1,
 			expect: []int{14, 10},
 		},
 		"case-2": {
 			nums:   []int{5, 6, 7},
+			n:      3,
 			k:      2,
 			expect: []int{7, 5},
 		},
 		"case-3": {
 			nums:   []int{5, 1, 3, 2, 4},
+			n:      5,
 			k:      2,
 			expect: []int{12, 6},
 		},
 		"case-4": {
 			nums:   []int{10, -1, -2, 5, 1},
+			n:      5,
 			k:      2,
 			expect: []int{16, -2},
 		},
 		"case-5": {
 			nums:   []int{1, 2, 3, 4, 5, 6},
+			n:      6,
 			k:      3,
 			expect: []int{15, 6},
 		},
 		"case-6": {
 			nums:   []int{7, 7, 7, 7},
+			n:      4,
 			k:      2,
 			expect: []int{14, 14},
 		},
 		"case-7": {
 			nums:   []int{0, -5, -10, 20, 30},
+			n:      5,
 			k:      1,
 			expect: []int{45, 5},
 		},
 		"case-8": {
 			nums:   []int{3, 1, 4, 1, 5, 9},
+			n:      6,
 			k:      3,
 			expect: []int{18, 5},
 		},
 		"case-9": {
 			nums:   []int{},
+			n:      0,
 			k:      3,
 			expect: []int{},
 		},
 		"case-10": {
 			nums:   []int{100, -1},
+			n:      2,
 			k:      2,
 			expect: []int{},
 		},
@@ -131,7 +188,7 @@ func RunTestMaxMinSums() {
 
 	for name, testCase := range testCases {
 		fmt.Printf("RUN %s\n", name)
-		result := maxMinSums(testCase.nums, testCase.k)
+		result := maxMinSums(testCase.nums, testCase.n, testCase.k)
 		if !cmp.EqualSlices(result, testCase.expect) {
 			fmt.Printf("=== FAILED: expect = %v - got = %v\n", testCase.expect, result)
 			os.Exit(1)
