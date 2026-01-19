72dbc6d302fb99e1f1f0121f99e87662a67937c2
Arief Putra <sibuearief@gmail.com>
2026-01-19 14:02:06 +0700

    update doc comments

--- problems/leetcode/go/560_subarray_sum_equals_k.go
+++ problems/leetcode/go/560_subarray_sum_equals_k.go
@@ -15,6 +15,12 @@ import (
  * URL              : https://leetcode.com/problems/subarray-sum-equals-k
  * Description      : Given an array of integers nums and an integer k, return the total number of subarrays whose sum
  * 					  equals to k. A subarray is a contiguous non-empty sequence of elements within an array.
+ *
+ * 					  Constraints:
+ * 					  - 1 <= nums.length <= 2 * 10^4
+ * 					  - -1000 <= nums[i] <= 1000
+ * 					  - -10^7 <= k <= 10^7
+ *
  * Examples         :
  * 					  Example 1:
  * 					  Input: nums = [1,1,1], k = 2
